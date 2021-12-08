96931950a80cceea310e693c9b05d52a80416cfe
Aayush Maurya <aayushmaurya1099@gmail.com>
2021-12-08 19:44:48 +0530

    server side added

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 const http = require("http");
+const websocketServer = require("websocket").server;
 const express = require("express");
 const path = require("path");
 const app = express();
-
-const websocketServer = require("websocket").server
 
 const staticPath = path.join(__dirname, "../public");
 
 app.use(express.static(staticPath));
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname+"/public/index.html");
+    res.sendFile(__dirname + "/public/index.html");
 });
 
-app.listen(8080, ()=>console.log("listening to port 8080"));
+app.get("/javascript", (req, res) => {
+    res.sendFile(__dirname + "/public/app.js");
+});
+
+app.get("/css", (req, res) => {
+    res.sendFile(__dirname + "/public/style/app.css");
+});
+
+app.listen(9091, () => console.log("listening to port 9091"));
 
 
 const httpServer = http.createServer();
@@ -24,3 +31,90 @@ httpServer.listen(9090, () => {
 const wsServer = new websocketServer({
     "httpServer": httpServer
 });
+
+const clients = {};
+
+wsServer.on("request", request => {
+    const connection = request.accept(null, request.origin);
+    connection.on("open", () => console.log("connection opened"));
+    connection.on("close", () => console.log("connection closed"));
+    connection.on("message", message => {
+        console.log(message);
+        const result = JSON.parse(message.utf8Data);
+        
+        console.log(result);
+
+        if(result.method === "play")
+        {
+            const payload = {
+                "method": "play",
+                "player": result.player
+            }
+
+           for(const c of Object.keys(clients)){
+               clients[c].connection.send(JSON.stringify(payload));
+           }
+        }
+
+        if(result.method === "reset")
+        {
+            const payload = {
+                "method": "reset"
+            }
+
+            for(const c of Object.keys(clients)){
+                clients[c].connection.send(JSON.stringify(payload));
+            }
+        }
+
+        if(result.method === "changeScore")
+        {
+            const payload = {
+                "method": "changeScore",
+                "value": result.value
+            }
+
+            for(const c of Object.keys(clients)){
+                clients[c].connection.send(JSON.stringify(payload));
+            }
+        }
+
+        if(result.method === "over")
+        {
+            const payload = {
+                "method": "over"
+            }
+
+            for(const c of Object.keys(clients)){
+                clients[c].connection.send(JSON.stringify(payload));
+            }
+        }
+
+    });
+
+    // creating a new client id
+    const clientId = guid();
+
+    // pushing new client
+    clients[clientId] = {
+        "connection": connection
+    }
+
+    const payload = {
+        "method": "connect",
+        "clientId": clientId
+    }
+
+    connection.send(JSON.stringify(payload));
+});
+
+
+// functions for creating unique id
+
+function S4() {
+    return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+}
+
+function guid() {
+    return (S4() + S4() + "-" + S4() + "-4" + S4().substr(0, 3) + "-" + S4() + "-" + S4() + S4() + S4()).toLowerCase();
+}
--- public/app.js
+++ public/app.js
@@ -1,63 +1,124 @@
 
+let clientId = null;
 
-const p1={
-    score:0,
-    button:document.querySelector('#p1btn'),
-    display:document.querySelector('#p1Display')
+let winningScore = 5;
+let gameOver = false;
+
+let ws = new WebSocket("ws://localhost:9090");
+
+ws.onmessage = message => {
+    const response = JSON.parse(message.data);
+    console.log(response);
+
+    if (response.method === "connect") {
+        clientId = response.clientId;
+        console.log("Connected to server with clientId = ", clientId);
+    }
+
+    if (response.method === "play") {
+        if (response.player === "p1")
+            updateScores(p1, p2);
+        if (response.player === "p2")
+            updateScores(p2, p1);
+    }
+
+    if (response.method === "reset")
+        reset();
+
+    if (response.method === "changeScore") {
+        winningScore = response.value;
+        winningScoreSelector.value = response.value;
+        reset();
+    }
+
+    if (response.method === "over") {
+        gameOver = true;
+        p1.display.classList.add('has-text-success');
+        p2.display.classList.add('has-text-danger');
+        p1.button.disabled = true;
+        p2.button.disabled = true;
+    }
 }
-const p2={
-    score:0,
-    button:document.querySelector('#p2btn'),
-    display:document.querySelector('#p2Display')
+
+const p1 = {
+    score: 0,
+    button: document.querySelector('#p1btn'),
+    display: document.querySelector('#p1Display')
+}
+const p2 = {
+    score: 0,
+    button: document.querySelector('#p2btn'),
+    display: document.querySelector('#p2Display')
 }
 
 
 
-const resetBtn=document.querySelector('#reset');
-const winningScoreSelector=document.querySelector('#playto');
+const resetBtn = document.querySelector('#reset');
+const winningScoreSelector = document.querySelector('#playto');
 
-let winningScore=5;
-let gameOver=false;
-function updateScores(player,opponent){
-    if(!gameOver){
-        if(player.score!=winningScore){
-        player.score+=1;
-        if(player.score===winningScore)
-        {
-            gameOver=true;
-            player.display.classList.add('has-text-success');
-            opponent.display.classList.add('has-text-danger');
-            player.button.disabled=true;
-            opponent.button.disabled=true;
-            
+function updateScores(player, opponent) {
+    if (!gameOver) {
+        if (player.score != winningScore) {
+            player.score += 1;
+            if (player.score === winningScore) {
+                const payload = {
+                    "method": "over"
+                }
+
+                ws.send(JSON.stringify(payload));
+                // gameOver = true;
+                // player.display.classList.add('has-text-success');
+                // opponent.display.classList.add('has-text-danger');
+                // player.button.disabled = true;
+                // opponent.button.disabled = true;
+            }
+            player.display.textContent = player.score;
         }
-       player.display.textContent=player.score;
-       
-        }
-        
-}}
-p1.button.addEventListener('click',function(){
-   updateScores(p1,p2)
+    }
+}
+p1.button.addEventListener('click', function () {
+    const payload = {
+        "method": "play",
+        "player": "p1"
+    }
+
+    ws.send(JSON.stringify(payload));
+    // updateScores(p1, p2)
 })
 
-p2.button.addEventListener('click',function(){
-    updateScores(p2,p1)
-})
-winningScoreSelector.addEventListener('change',function(){
-   winningScore =parseInt(this.value);
-   reset();
-})
-resetBtn.addEventListener('click',reset)
-function reset()
-{ gameOver=false;
-    p1.score=0;
-    p2.score=0;
-    p1.display.textContent=0;
-    p2.display.textContent=0;
-    p1.display.classList.remove('has-text-success','has-text-danger');
- p2.display.classList.remove('has-text-danger','has-text-success');
- p1.button.disabled=false;
- p2.button.disabled=false;
+p2.button.addEventListener('click', function () {
+    const payload = {
+        "method": "play",
+        "player": "p2"
+    }
 
+    ws.send(JSON.stringify(payload));
+    // updateScores(p2, p1)
+})
+winningScoreSelector.addEventListener('change', function () {
+    const payload = {
+        "method": "changeScore",
+        "value": parseInt(this.value)
+    }
 
+    ws.send(JSON.stringify(payload));
+    // winningScore = parseInt(this.value);
+    // reset();
+})
+resetBtn.addEventListener('click', function () {
+    const payload = {
+        "method": "reset"
+    }
+    ws.send(JSON.stringify(payload));
+});
+function reset() {
+    gameOver = false;
+    p1.score = 0;
+    p2.score = 0;
+    p1.display.textContent = 0;
+    p2.display.textContent = 0;
+    p1.display.classList.remove('has-text-success', 'has-text-danger');
+    p2.display.classList.remove('has-text-danger', 'has-text-success');
+    p1.button.disabled = false;
+    p2.button.disabled = false;
 }
